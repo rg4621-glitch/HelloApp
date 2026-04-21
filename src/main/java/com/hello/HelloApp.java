ab5375d1cff6da73166bdb12a13af4fe52319517
rakesh-gandavaram <rakeshgandavaram2008@gmail.com>
2026-04-21 15:35:42 +0530

    uc4 implement'

--- src/main/java/com/hello/HelloApp.java
+++ src/main/java/com/hello/HelloApp.java
@@ -1,14 +1,29 @@
 package com.hello;
+
 public class HelloApp {
+    public static void main(String[] args) {
+        String names;
 
-public static void main(String[] args) {
+        
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            
+           
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                
+               
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            names = nameBuilder.toString();
+        } else {
+            
+            names = "World";
+        }
 
-String name = "World";
-
-if (args.length > 0) {
-
-name = args[0];
-}
-System.out.println("Hello, " + name + "!");
- }
+        
+        System.out.println("Hello, " + names + "!");
+    }
 }
