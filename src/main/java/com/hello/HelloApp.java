662735a9067edd285f5d8d33421e31416eed39cd
rakesh-gandavaram <rakeshgandavaram2008@gmail.com>
2026-04-22 10:28:09 +0530

    uc7 implemented

--- src/main/java/com/hello/HelloApp.java
+++ src/main/java/com/hello/HelloApp.java
@@ -1,7 +1,16 @@
 package com.hello;
 
 public class HelloApp {
-		public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		}
+    public static void main(String[] args) {
+        
+        String names = "World";
+
+        
+        if (args.length > 0) {
+            names = String.join(", ", args);
+        }
+
+        
+        System.out.println("Hello, " + names + "!");
+    }
 }
